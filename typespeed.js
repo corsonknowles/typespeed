6f1c0184ad53a9cf34b048fa7ae090bb5d3a20b0
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-09-13 13:16:33 -0700

    Initialize repo with index and js file

---
+++ typespeed.js
@@ -0,0 +1,17 @@
+
+
+let begun = false;
+
+
+// TODO put this inside event listener for text in input field
+if (!begun) {
+  begun = true;
+  let startTime = new Date();
+}
+
+// TODO put this inside calculator for completed words
+if () {
+  let endTime = new Date();
+}
+
+let elapsedTime = endTime - startTime;
