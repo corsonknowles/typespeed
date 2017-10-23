bd156e2e4ce3a16eff01daf9e65835e6f1f87cb4
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-10-23 13:56:22 -0700

    Add event listeners

--- typespeed.js
+++ typespeed.js
@@ -1,17 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  let begun = false;
 
 
-let begun = false;
+  // TODO put this inside event listener for text in input field
+  document.getElementByID("type-speed-input").oninput = () => {
+    if (!begun) {
+      begun = true;
+      let startTime = new Date();
+    }
+  }
+
+  // TODO put this inside calculator for completed words
+  if () {
+    let endTime = new Date();
+  }
+
+  let elapsedTime = endTime - startTime;
 
 
-// TODO put this inside event listener for text in input field
-if (!begun) {
-  begun = true;
-  let startTime = new Date();
-}
-
-// TODO put this inside calculator for completed words
-if () {
-  let endTime = new Date();
-}
-
-let elapsedTime = endTime - startTime;
+//end of doc, end of document loaded listener
+});
