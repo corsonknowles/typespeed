6c7536d5b3b63940f5303c042353f145cdfef05f
casey fritsch <caseyfritsch@gmail.com>
2017-10-25 17:11:34 -0700

    added updateText and elasped time

--- typespeed.js
+++ typespeed.js
@@ -3,15 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
   let startTime;
   let endTime;
   let wordCount;
+  let elapsedTime;
+  let testLength;
+  let texts = ['HI','BYE','CEY'];
+  let completed = false;
+  let currentText = texts[Math.floor(Math.random() * texts.length)];
+  console.log(currentText);
+
   let accuracyNode = document.getElementById("accuracy");
   let countNode = document.getElementById("word-count");
   let errorNode = document.getElementById("error-count");
+  let elapsedTimeNode = document.getElementById("typing-time");
+
 
   let stringCount = (string) => string.split(' ').filter( e => e ).length;
   let displayField = document.getElementById("display");
-  let totalWords = stringCount(displayField.value);
+  // let totalWords = stringCount(displayField.innerHTML);
   let inputField = document.getElementById("type-speed-input");
 
+  displayField.innerHTML = currentText;
+  testLength = stringCount(currentText);
 
   inputField.oninput = () => {
     if (!begun) {
@@ -29,14 +40,28 @@ document.addEventListener('DOMContentLoaded', () => {
     // console.log(inputField.value);
     // console.log(wordCount);
 
-    if (wordCount) {
+    if (wordCount === testLength ) {
       endTime = new Date();
     }
 
-    if (endTime) {
-      let elapsedTime = endTime - startTime;
+    let userInput = inputField.value;
+    console.log('userinput', userInput);
+    let lastIdx = userInput.split(' ').length - 1;
+    console.log('lastIdx', lastIdx);
+    let lastword = userInput.split(' ')[lastIdx];
+    console.log('lastword', lastword);
+    let lastWordsMatch = (lastword === currentText.split(" ")[lastIdx]);
+    console.log('target', currentText.split(" ")[lastIdx]);
+    if (endTime && !completed && lastWordsMatch ) {
+      elapsedTime = endTime - startTime;
+      elapsedTimeNode.innerHTML = elapsedTime;
+      completed = true;
     }
-  }
+  };
+
+  let updateText = (newText) => {
+    displayField.innerHTML = newText;
+  };
 
 //end of doc, end of document loaded listener
 });
